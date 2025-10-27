7f77a9ee976c858faea6c2a8cb4bb7d92ce3a2c9
zhu hao <1815526897@qq.com>
2025-10-27 20:35:11 +0800

    提交说明

--- tune_big.py
+++ tune_big.py
@@ -1,5 +1,4 @@
 import time
-
 import optuna
 import torch
 import random
@@ -126,12 +125,13 @@ def objective(trial):
 
     # ---- 3. 运行实验 ----
     # 设置 GPU
+    args.use_gpu = True if torch.cuda.is_available() and args.use_gpu else False
+
     if args.use_gpu and args.use_multi_gpu:
         args.devices = args.devices.replace(' ', '')
         device_ids = args.devices.split(',')
         args.device_ids = [int(id_) for id_ in device_ids]
         args.gpu = args.device_ids[0]
-
     # 实例化实验
     exp = Exp_Long_Term_Forecast(args)
 
