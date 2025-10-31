fec05898c107307e03203d68ef4aed5337ed6054
zhu hao <1815526897@qq.com>
2025-10-31 15:56:13 +0800

    1

--- tune3.py
+++ tune3.py
@@ -97,7 +97,7 @@ def objective(trial):
     # Optuna 将从这里动态地建议超参数，覆盖默认值
     args = parser.parse_args()  # 使用空列表来避免解析命令行
 
-    args.learning_rate = trial.suggest_float('learning_rate', 1e-4, 3e-4, log=True)
+    args.learning_rate = trial.suggest_float('learning_rate', 7e-5, 2e-4, log=True)
     args.batch_size = trial.suggest_categorical('batch_size', [16,32,48])
 
     # # 学习率调度器
@@ -105,12 +105,12 @@ def objective(trial):
     args.pd_layers = 1
     args.ia_layers = trial.suggest_categorical('ia_layers', [1,2])
 
-    possible_n_heads = [h for h in [4,8,16,32,64] if args.d_model % h == 0]
+    possible_n_heads = [h for h in [8,16,32,64] if args.d_model % h == 0]
     if not possible_n_heads:  # 如果没有可用的 n_heads，则跳过此次试验
         raise optuna.exceptions.TrialPruned()
     args.n_heads = trial.suggest_categorical('n_heads', possible_n_heads)
     #args.num_p = trial.suggest_categorical('num_p', [4,6,8,12])
-    args.alpha = trial.suggest_float('alpha', 0.04, 0.13, log=True)
+    args.alpha = trial.suggest_float('alpha', 0.02, 0.1, log=True)
     # # d_ff 通常是 d_model 的倍数
     #args.d_ff = trial.suggest_categorical('d_ff_multiplier', [1, 2, 4]) * args.d_model
 
@@ -166,7 +166,7 @@ if __name__ == '__main__':
 
     # 'n_trials' 是你想要尝试的超参数组合的总次数
     # 从一个较小的数字开始，比如 20，然后再增加
-    study.optimize(objective, n_trials=10)
+    study.optimize(objective, n_trials=8)
 
     # ---- 6. 输出优化结果 ----
     print("\n\n--- 优化完成 ---")
