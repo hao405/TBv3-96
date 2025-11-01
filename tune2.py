cf2c7b852afd83f8fe7a398780d5f3772be6d860
zhu hao <1815526897@qq.com>
2025-11-01 11:10:01 +0800

    1

--- tune2.py
+++ tune2.py
@@ -106,8 +106,6 @@ def objective(trial):
         args.ca_layers = trial.suggest_categorical('ca_layers', [1])
         args.pd_layers = 1
         args.ia_layers = trial.suggest_categorical('ia_layers', [1,2])
-        args.d_model = trial.suggest_categorical('d_model', [64, 128, 256 ,512])
-        args.d_ff = args.d_model
         args.alpha = trial.suggest_float('alpha', 0.05, 0.40, log=True)
     elif args.data_path == 'ETTm2.csv':
         args.ca_layers = trial.suggest_categorical('ca_layers', [1])
@@ -118,8 +116,6 @@ def objective(trial):
         args.ca_layers = trial.suggest_categorical('ca_layers', [0,1])
         args.pd_layers = 1
         args.ia_layers = trial.suggest_categorical('ia_layers', [2,3])
-        args.d_model = trial.suggest_categorical('d_model', [64, 128, 256 ,512])
-        args.d_ff = args.d_model
         args.alpha = trial.suggest_float('alpha', 0.30, 0.40, log=True)
     else:
         args.ca_layers = trial.suggest_categorical('ca_layers', [0])
