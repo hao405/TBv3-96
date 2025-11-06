6ef8d6970d0add35d7ff25503dc8e0e34d5ac450
zhmac <1815526897@qq.com>
2025-11-06 10:01:38 +0800

    a

--- tune3.py
+++ tune3.py
@@ -26,7 +26,7 @@ def objective(trial):
     # ablation control flags
     parser.add_argument('--revin', action='store_false', help='non-stationary for short-term', default=True)
     parser.add_argument('--alpha', type=float, default=0.2, help='weight of time-frequency MAE loss')
-    parser.add_argument('--beta', type=float, default=0.1, help='weight of mse loss')
+    parser.add_argument('--beta', type=float, default=0, help='weight of mse loss')
     parser.add_argument('--dropout', type=float, default=0.0, help='dropout')
     parser.add_argument('--attn_dropout', type=float, default=0.15, help='dropout')
     parser.add_argument('--batch_size', type=int, default=32, help='batch size of train input data')
@@ -98,21 +98,21 @@ def objective(trial):
     # Optuna 将从这里动态地建议超参数，覆盖默认值
     args = parser.parse_args()  # 使用空列表来避免解析命令行
 
-    args.learning_rate = trial.suggest_float('learning_rate', 1e-4, 4e-4, log=True)
-    args.batch_size = trial.suggest_categorical('batch_size', [16,32,64])
+    args.learning_rate = trial.suggest_float('learning_rate', 1e-4, 5e-4, log=True)
+    args.batch_size = trial.suggest_categorical('batch_size', [16])
 
     # # 学习率调度器
     args.ca_layers = trial.suggest_categorical('ca_layers', [1])
     args.pd_layers = 1
     args.ia_layers = trial.suggest_categorical('ia_layers', [1])
 
-    possible_n_heads = [h for h in [4,32,64] if args.d_model % h == 0]
+    possible_n_heads = [h for h in [4] if args.d_model % h == 0]
     if not possible_n_heads:  # 如果没有可用的 n_heads，则跳过此次试验
         raise optuna.exceptions.TrialPruned()
     args.n_heads = trial.suggest_categorical('n_heads', possible_n_heads)
     #args.num_p = trial.suggest_categorical('num_p', [4,6,8,12])
-    args.alpha = trial.suggest_float('alpha', 0.00001 , 0.08, log=True)
-    args.beta = trial.suggest_float('beta', 0.000005 , 0.08, log=True)
+    args.alpha = trial.suggest_float('alpha', 0.00001 , 0.11, log=True)
+ 
     # # d_ff 通常是 d_model 的倍数
     #args.d_ff = trial.suggest_categorical('d_ff_multiplier', [1, 2, 4]) * args.d_model
 
