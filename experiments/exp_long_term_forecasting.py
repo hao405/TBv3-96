f1bd75a5663004b70d64c84b4e52d9d646e72cff
zhmac <1815526897@qq.com>
2025-12-09 11:36:22 +0800

    first commit

--- experiments/exp_long_term_forecasting.py
+++ experiments/exp_long_term_forecasting.py
@@ -270,7 +270,7 @@ class Exp_Long_Term_Forecast(Exp_Basic):
         mae, mse, rmse, mape, mspe = metric(preds, trues)
         print('mse:{}, mae:{}'.format(mse, mae))
         print('rmse:{}, mape:{}, mspe:{}'.format(rmse, mape, mspe))
-        f = open("result_long_term_forecast.txt", 'a')
+        f = open("solar192to720_result_long_term_forecast.txt", 'a')
         f.write(setting + "  \n")
         f.write('mse:{}, mse:{}, rmse:{}, mape:{}, mspe:{}'.format(mse, mae, rmse, mape, mspe))
         f.write('\n')
